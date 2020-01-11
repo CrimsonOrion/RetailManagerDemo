f863b5bc875ae06bb04cf216661b59a70925f534
James Lynch <eimi_christian@hotmail.com>
2020-01-11 12:08:18 -0500

    Initialized WPF UI and Library

---
+++ RMDesktop.UI/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+
+using RMDesktop.UI.ViewModels;
+
+using System.Windows;
+
+namespace RMDesktop.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper() => Initialize();
+
+        protected override void OnStartup(object sender, StartupEventArgs e) => DisplayRootViewFor<ShellViewModel>();
+    }
+}
