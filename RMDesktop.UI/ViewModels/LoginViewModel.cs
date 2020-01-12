4fcd1f081c782f7e087305e7e5d6876d38cec8f0
James Lynch <eimi_christian@hotmail.com>
2020-01-12 13:25:46 -0500

    Lesson 12: Error Handling LogIn Form.

--- RMDesktop.UI/ViewModels/LoginViewModel.cs
+++ RMDesktop.UI/ViewModels/LoginViewModel.cs
@@ -10,22 +10,28 @@ namespace RMDesktop.UI.ViewModels
         private string _username;
         private string _password;
         IAPIHelper _apiHelper;
+        private string _errorMessage;
 
         public LoginViewModel(IAPIHelper apiHelper)
         {
             _apiHelper = apiHelper;
         }
 
-        public string Username
-        {
-            get => _username;
-            set { _username = value; NotifyOfPropertyChange(() => Username); }
+        public string Username { get => _username; set { _username = value; NotifyOfPropertyChange(() => Username); } }
+        public string Password { get => _password; set { _password = value; NotifyOfPropertyChange(() => Password); NotifyOfPropertyChange(() => CanLogIn); } }
+        public bool IsErrorVisible
+        { 
+            get 
+            {
+                var output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
         }
-        public string Password
-        {
-            get => _password;
-            set { _password = value; NotifyOfPropertyChange(() => Password); NotifyOfPropertyChange(() => CanLogIn); }
-        }
+        public string ErrorMessage { get => _errorMessage; set { _errorMessage = value; NotifyOfPropertyChange(() => ErrorMessage); NotifyOfPropertyChange(() => IsErrorVisible); } }
 
         public bool CanLogIn => Username?.Length > 0 && Password?.Length > 0;
 
@@ -34,11 +40,12 @@ namespace RMDesktop.UI.ViewModels
             try
             {
                 var result = await _apiHelper.Authenticate(Username, Password);
+                ErrorMessage = string.Empty;
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-            } 
+                ErrorMessage = ex.Message;
+            }
         }
     }
 }
